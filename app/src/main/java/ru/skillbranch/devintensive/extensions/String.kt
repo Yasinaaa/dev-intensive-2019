e0ca8646ea74f0a73a084b129f65b62eed0608c9
Zalyalova Yasina <zalyalova@infomatika.ru>
2020-01-03 21:13:02 +0300

    Add String extensions

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -3,3 +3,8 @@ package ru.skillbranch.devintensive.extensions
 /*
  * Created by yasina on 2020-01-03
 */
+fun String.truncate(length: Int = 16)
+        = if (this.trim().length <= length) this.trim() else this.trim().substring(0, length).trim() + "..."
+
+fun String.stripHtml()
+        = this.replace(Regex("(<.*?>)|(&[^ а-я]{1,4}?;)"), "").replace(Regex(" {2,}"), " ")
